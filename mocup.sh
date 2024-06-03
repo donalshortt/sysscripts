75482e957411bb30f20b8c78b20373ce65471534
donalshortt <donalshortt@donals.eu>
2024-06-03 23:53:45 +0200

    added quick launch of moc dev environment

--- mocup.sh
+++ mocup.sh
@@ -17,11 +17,17 @@ main()
     done
     shift "$((OPTIND - 1))"
 
-	kitten @ launch --type=tab --tab-title "Front" --keep-focus zsh
-	kitten @ launch --type=tab --tab-title "Back" --keep-focus zsh
-	kitten @ launch --type=tab --tab-title "Parser" --keep-focus zsh
-	echo "launched??"
+    # Launching tabs with appropriate commands and keeping the shell open
+    kitten @ launch --type=tab --tab-title "Front" --keep-focus sh -c 'cd /home/donal/projects/mocf && git pull && nvim; exec $SHELL'
+    kitten @ launch --type=tab --tab-title "Back" --keep-focus sh -c 'cd /home/donal/projects/mocb && git pull && nvim; exec $SHELL'
+    kitten @ launch --type=tab --tab-title "Parser" --keep-focus sh -c 'cd /home/donal/projects/mocp && git pull && nvim; exec $SHELL'
+	kitten @ launch --type=tab --tab-title "Savefile" --keep-focus sh -c 'cd "/home/donal/.local/share/Paradox Interactive/Europa Universalis IV/save games" && nvim mp_autosave.eu4; exec $SHELL'
 
+    # Launching Kitty with a specific layout and running commands, keeping the shell open
+    kitty -o allow_remote_control=yes -o enabled_layouts=tall sh -c \
+        'kitten @ launch --type=tab --tab-title "Front" --keep-focus sh -c "cd /home/donal/projects/mocf && npm run dev; exec $SHELL" && \
+         kitten @ launch --type=tab --tab-title "Back" --keep-focus sh -c "cd /home/donal/projects/mocb && nodemon app.js; exec $SHELL" && \
+         kitten @ launch --type=tab --tab-title "Parser" --keep-focus sh -c "cd /home/donal/projects/mocp && cargo run; exec $SHELL"' &
 }
 
 main "${@}"
